dbd156028fd07433b043926611b18a89422c9c48
lionard <leonard.muhati@student.moringaschool.com>
2025-11-04 10:50:34 +0300

    let us see if it works

--- sellpal-escrow-server/server.js
+++ sellpal-escrow-server/server.js
@@ -117,7 +117,8 @@ app.post("/api/checkout", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 SellPal Escrow backend running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
